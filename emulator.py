591b283f8023d5b5e56e9877f4f71a5690443aba
Yuvi-App <admin@yuvi.app>
2020-01-02 15:54:29 -0500

    Updated Region Server List code to fix a  Minor bug. where it was pushing same blocklist to all regions.

--- emulator.py
+++ emulator.py
@@ -231,17 +231,17 @@ class Server(object):
 
         regiontotalNA, blockslistNA = self.GhostManager.get_current_players(SERVER_PORT_US)
         MapNA = "Popular areas in North America:\r\n"
-        for count, blockID in blockslist[::-1][0:15]:
+        for count, blockID in blockslistNA[::-1][0:15]:
             MapNA += "%4d %s\r\n" % (count, blocknames[blockID])
 
         regiontotalEU, blockslistEU = self.GhostManager.get_current_players(SERVER_PORT_EU)
         MapEU = "Popular areas in Europe:\r\n"
-        for count, blockID in blockslist[::-1][0:15]:
+        for count, blockID in blockslistEU[::-1][0:15]:
             MapEU += "%4d %s\r\n" % (count, blocknames[blockID])
 
         regiontotalJP, blockslistJP = self.GhostManager.get_current_players(SERVER_PORT_JP)
         MapJP = "Popular areas in Japan:\r\n"
-        for count, blockID in blockslist[::-1][0:15]:
+        for count, blockID in blockslistJP[::-1][0:15]:
             MapJP += "%4d %s\r\n" % (count, blocknames[blockID])
 
         # first byte
